ce0c440bec1c36556a10080c848ddba7f896361c
Camilo Monsalve Gómez <beren.monsalve@gmail.com>
2020-03-28 11:22:24 -0500

    human behaviour (#4)
    * human behaviour
    
    * tags and scene

---
+++ Y Fuera/Assets/prefab_camilo/script/Human.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    public bool fallen = false;
+    public float distance = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("enter Collider");
+        if (other.gameObject.CompareTag("object")) {
+            Debug.Log("tag object");
+            ObjectObstacle obj = other.gameObject.GetComponent<ObjectObstacle>();
+            if (obj != null) {
+                Debug.Log(obj.Type);
+                switch (obj.Type){
+                    case ObjectType.UP:
+                        this.fallen = false;
+                        break;
+                    case ObjectType.DOWN:
+                        this.fallen = true;
+                        break;
+                    case ObjectType.MOVE:
+                        this.distance = obj.distance;
+                        break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Y Fuera/Assets/prefab_camilo/script/ObjectObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectType {
+    UP,
+    DOWN,
+    MOVE
+}
+
+public class ObjectObstacle : MonoBehaviour
+{
+    public ObjectType Type;
+    public float distance = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
