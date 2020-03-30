04d9f7c1c91e7ad44b20c563548f8bd922d94058
Edison Zapata <zed15600@gmail.com>
2020-03-29 20:18:40 -0500

    Added Script to make the camera to follow the human (#19)
    Co-authored-by: Edison Zapata <edison@austinsoftware.com>

---
+++ Y Fuera/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform human;
+    public float leftEdge;
+    public float rightEdge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 initialPos = new Vector3(leftEdge, transform.position.y, transform.position.z);
+        transform.position = initialPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (human.position.x > leftEdge && human.position.x < rightEdge) {
+            transform.position = new Vector3(human.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
